e6d80e20f449760f8c3b4c094cdf29f74479da63
Lucasqwq <103513284+Lucasqwq@users.noreply.github.com>
2022-06-23 22:05:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,16 @@ import requests , json
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+
+with open('GOVERNMENT_KEY.txt','r') as f:
+ urlapi = f.read()
  
 cred = credentials.Certificate("private_key.json") # 引用私密金鑰
 firebase_admin.initialize_app(cred)                                     # 初始化firebase，注意不能重複初始化
 db = firestore.client()                                                 # 初始化firestore
  
 def get_weather_into_firebase():                                        #從url中拿取資料，並寫入到資料庫
-    url = "https://opendata.cwb.gov.tw/api/v1/rest/datastore/{}?Authorization=CWB-942996D3-4FEE-4CC0-B0F8-0F5A6E5CB9E9&format=JSON"  #擷取資料的url
+    url = "https://opendata.cwb.gov.tw/api/v1/rest/datastore/{}?Authorization="+urlapi+"&format=JSON"  #擷取資料的url
     country_code = {"宜蘭縣":"F-D0047-001","桃園市":"F-D0047-005","新竹縣":"F-D0047-009","苗栗縣":"F-D0047-013","彰化縣":"F-D0047-017","南投縣":"F-D0047-021","雲林縣":"F-D0047-025","嘉義縣":"F-D0047-029","屏東縣":"F-D0047-033","臺東縣":"F-D0047-037","花蓮縣":"F-D0047-041","澎湖縣":"F-D0047-045","基隆市":"F-D0047-049","新竹市":"F-D0047-053","嘉義市":"F-D0047-057","臺北市":"F-D0047-061","高雄市":"F-D0047-065","新北市":"F-D0047-069","臺中市":"F-D0047-073","臺南市":"F-D0047-077","連江縣":"F-D0047-081","金門縣":"F-D0047-085"} #縣市的網頁代碼
     for code_number in country_code:
         get_data_url = url.format(country_code[code_number])            #把不同縣市的代碼輸入到url裡
